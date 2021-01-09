674b7e9da41a1c5177eb1d49a3d2233a6e6eb643
Mullakhashim <mullahashimsimai@gmail.com>
2021-01-09 12:54:38 -0500

    created login functionalities for all users including two HR, two marketing and two helpdesk users

---
+++ src/main/java/UserStory1/Login.java
@@ -0,0 +1,114 @@
+package UserStory1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+// comments
+public static void main(String[] args) {
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver=new ChromeDriver();
+    driver.manage().window().maximize();
+    // logInAsHR51(driver);
+    // logInAsHR52(driver);
+    // logInAsHelpDesk51(driver);
+    // logInAsHelpDesk52(driver);
+    // logInAsMarketing51(driver);
+    // logInAsMarketing52(driver);
+
+
+}
+    // static method logInAsHR51 will accept a WebDriver parameter...
+    // and after execution will login to "https://login2.nextbasecrm.com" as a "hr51" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsHR51(driver); **********
+    public static void logInAsHR51(WebDriver driver){
+    driver.get("https://login2.nextbasecrm.com");
+    driver.findElement(By.xpath("//input[@type='text']")).sendKeys("hr51@cybertekschool.com");
+    driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+    WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+    checkboxRememberMe.click();
+    driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+}
+
+    // static method logInAsHR52 will accept a WebDriver parameter and after execution...
+    // will login to "https://login2.nextbasecrm.com" as a "hr52" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsHR52(driver); **********
+    public static void logInAsHR52(WebDriver driver){
+        driver.get("https://login2.nextbasecrm.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("hr52@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+        checkboxRememberMe.click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+    }
+
+    // static method logInAsHelpDesk51 will accept a WebDriver parameter and after...
+    // execution will login to "https://login2.nextbasecrm.com" as a "helpdesk51" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsHelpDesk51(driver); **********
+    public static void logInAsHelpDesk51(WebDriver driver){
+        driver.get("https://login2.nextbasecrm.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("helpdesk51@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+        checkboxRememberMe.click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+    }
+
+    // static method logInAsHelpDesk52 will accept a WebDriver parameter and after...
+    // execution will login to "https://login2.nextbasecrm.com" as a "helpdesk52" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsHelpDesk52(driver); **********
+    public static void logInAsHelpDesk52(WebDriver driver){
+        driver.get("https://login2.nextbasecrm.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("helpdesk52@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+        checkboxRememberMe.click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+    }
+
+    // static method logInAsMarketing51 will accept a WebDriver parameter and after...
+    // execution will login to "https://login2.nextbasecrm.com" as a "marketing51" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsMarketing51(driver); **********
+    public static void logInAsMarketing51(WebDriver driver){
+        driver.get("https://login2.nextbasecrm.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("marketing51@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+        checkboxRememberMe.click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+    }
+
+    // static method logInAsMarketing52 will accept a WebDriver parameter and after...
+    // execution will login to "https://login2.nextbasecrm.com" as a "marketing52" user
+    // ****** IT HAS TO BE CALLED BY PACKAGE NAME AND CLASS NAME ******
+    // ****** UserStory1.Login.logInAsMarketing52(driver); **********
+    public static void logInAsMarketing52(WebDriver driver){
+        driver.get("https://login2.nextbasecrm.com");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("marketing52@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        WebElement checkboxRememberMe= driver.findElement(By.xpath("//input[@type='checkbox']"));
+        checkboxRememberMe.click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+    }
+
+
+
+
+
+
+}

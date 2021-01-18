b88dd28d2fffece5aed296ab58ed7ad0b5b3457d
gstorohuk <galyna.storozhuk@gmil.com>
2021-01-18 13:08:32 -0500

    More random classes

--- src/main/java/UserStory4/Random/RandomClass.java
+++ src/main/java/UserStory4/Random/RandomClass.java
@@ -25,8 +25,14 @@ public class RandomClass {
                 "Difference between Character Constant and String Constant in java ?", "What are constants and how to create constants in java?", "Difference between ‘>>’ and ‘>>>’ operators in java?",
                 "What is data driven testing", "What is the approach of counting Sprints at your company ", "What does include in your test plan? Is it good for one sprint, or 1 release? 1 quarter? ",
                 "what is use case", "is edge test case and boundary test case are the same? ", "What is ad hoc testing?", "What is the difference between using super and super () in a Java method?",
-                "Difference between no such element exception ans Stale element exception", "What is sanity testing? Difference between sanity and regression testing", "What does <rat hole> mean in a casual Agile team conversation"));
-        ArrayList<String> namesOfGroupmates = new ArrayList<>(Arrays.asList("Vadym", "Karolina", "Simai", "Polina", "Oleg", "Galyna"));
+                "Difference between no such element exception ans Stale element exception", "What is sanity testing? Difference between sanity and regression testing", "What does <rat hole> mean in a casual Agile team conversation",
+                "Why java is not 100% object oriented?", "What is an abstract class?", "an we declare a class as Abstract without having any abstract method?", "Does Importing a package imports its sub-packages as well in Java?",
+                "Is it necessary for a Try Block to be followed by a Catch Block in Java for Exception handling?", "Is there any way to skip Finally block of exception even if some exception occurs in the exception block?",
+               "Can a class be a super class and a sub-class at the same time? Give example", "How can we restrict inheritance for a class so that no class can be inherited from it?", "How can we use primitive data types as objects?",
+                "Is JDK required on each machine to run a Java program?", "What's the difference between comparison done by equals method and == operator?", "Can a variable be local and static at the same time?",
+                "In a class implementing an interface, can we change the value of any variable defined in the interface?", "I have multiple constructors defined in a class. Is it possible to call a constructor from another constructor's body?",
+                "What's the base class of all exception classes?"));
+        ArrayList<String> namesOfGroupmates = new ArrayList<>(Arrays.asList("Jeren", "Inci", "Galyna", "Esma"));
 
         Random random1 = new Random();
         Random random2 = new Random();

1f1c15248374d57dc46ea32e59566ff5b839c68b
Vadimiershov <40811461+Vadimiershov@users.noreply.github.com>
2021-01-20 17:21:13 -0600

    CRM folder updated

--- src/test/java/CRMNextBaseProject/Runner.java
+++ src/test/java/CRMNextBaseProject/Runner.java
@@ -1,8 +1,10 @@
 package CRMNextBaseProject;
 
+import UserStory1.Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.net.Urls;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,40 +17,20 @@ public class Runner {
     public void Login() {
         Driver.setUpDriver();
         WebDriver localChromeDriver = Driver.getDriver();
-        Urls url = new Urls();
-        localChromeDriver.get(url.nextbasecrm);
-        Users users = new Users();
-        Login loginPage = new Login();
-        loginPage.login();
-// Setting up expected and actual
-        String expectedUsername = users.user51;
-        HomePage homePage = new HomePage();
-        String actualUsername = homePage.loggedInUsername.getText();
-// Asserting values
-        Assert.assertEquals(actualUsername, expectedUsername, "Your actual username was different than expected!!!!!");
+        Data url = new Data();
+        localChromeDriver.get(url.url);
+        Data users = new Data();
+        LogIn loginPage = LogIn.getInstance();
+        loginPage.loginHR51();
+
+
 
        Driver.killDriver();
     }
 
-    @Test(description = "One single scenario to create poll")
-    public void Poll() {
-        Driver.setUpDriver();
-        WebDriver localChromeDriver = Driver.getDriver();
-        Urls url = new Urls();
-        localChromeDriver.get(url.nextbasecrm);
-        Login loginPage = new Login();
-        loginPage.login();
-// Created poll
-        HomePage homePage = new HomePage();
-        homePage.poll.click();
-        ThreadSleep.threadSleep(2);
 
-        localChromeDriver.switchTo().frame(0);
-        WebElement pollMessage = localChromeDriver.findElement(By.xpath("//body[@contenteditable='true']"));
-        pollMessage.clear();
-        pollMessage.sendKeys("Some message");
 
 
     }
 
-}
+
--- src/test/java/CRMNextBaseProject/Users.java
+++
@@ -1,9 +0,0 @@
-package CRMNextBaseProject;
-
-public class Users {
-
-    public final String user51 = "hr51@cybertekschool.com";
-    public final String user52 = "hr52@cybertekschool.com";
-
-    public final String password = "UserUser";
-}

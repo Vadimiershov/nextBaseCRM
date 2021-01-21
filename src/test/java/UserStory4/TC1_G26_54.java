d8d6b9405c3e857a19e48963166fd1cfbe681f10
gstorohuk <galyna.storozhuk@gmil.com>
2021-01-21 13:07:38 -0500

    Merge branch 'Vadym' of https://github.com/Vadimiershov/nextBaseCRM into gstorozhuk
     Conflicts:
    	src/main/java/UserStory1/Login.java
    	src/main/java/UserStory5/Hr.java
    	target/classes/UserStory5/Hr.class

--- src/test/java/UserStory4/TC1_G26_54.java
+++ src/test/java/UserStory4/TC1_G26_54.java
@@ -1,21 +1,64 @@
 package UserStory4;
 
-import UserStory5.Driver;
-import UserStory5.LogIn;
-import UserStory5.LogOut;
-import UserStory5.PollCreating;
+import CRMNextBaseProject.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TC1_G26_54 extends LogIn{
+public class TC1_G26_54 {
 
 
-@BeforeMethod
-public void setUpDriverGetUrl(){
+    @BeforeMethod(description = "setting up driver")
+    public void setUpDriver1(){
+        CRMNextBaseProject.Driver.setUpDriver();
+        CRMNextBaseProject.Driver.navigateToURL(Data.url);
+
+    }
+
+    @Test(description = "Verify users can write a poll message title with question & answer US4 TOD G26-46")
+    public void Login() {
+        LogIn loginPage = LogIn.getInstance();
+        loginPage.loginHR51();
+     // Poll creation for HR51
+
+        HomePage homePage = new HomePage();
+        homePage.pollButton.click();
+        ThreadSleep.threadSleep(5);
+        Driver.getDriver().switchTo().frame(0);
+
+      //  homePage.messagePoll.clear();
+
+        homePage.messagePoll.sendKeys("Test Case Automation");
+        Driver.getDriver().switchTo().parentFrame();
+        homePage.pollQuestion.sendKeys("Why is it taking soooo long to automate");
+
+        homePage.answer1.clear();
+        homePage.answer1.sendKeys("Study more");
+
+        homePage.answer2.clear();
+        homePage.answer2.sendKeys("Let it go");
+
+        homePage.answer3.clear();
+        homePage.answer3.sendKeys("Whatever");
 
 
-}
-}
+
+
+
+
+
+
+
+
+    }
+
+   // @AfterMethod
+   // public void tearDown(){
+    //    Driver.killDriver();
+    }
+
+
+
+
 
 

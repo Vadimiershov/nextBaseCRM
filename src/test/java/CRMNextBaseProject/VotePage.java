6e0b9ffff16826f0b7dcf2be22f0121c014d172b
Vadimiershov <40811461+Vadimiershov@users.noreply.github.com>
2021-01-24 13:11:22 -0600

    UserStory 5 done

--- src/test/java/CRMNextBaseProject/VotePage.java
+++ src/test/java/CRMNextBaseProject/VotePage.java
@@ -11,8 +11,31 @@ public class VotePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(how = How.XPATH, using = "(//span[@class='feed-add-post-form-link'])[3]//span")
-    public WebElement pollButton;
+    @FindBy(how = How.XPATH, using = "//span[.='Study more']//label")
+    public WebElement answerRadio1; // radio button to select answer "Study more"
+
+    @FindBy(how = How.XPATH, using = "//span[.='Let it go']//label[1]")
+    public WebElement answerRadio2; // radio button to select answer "Let it go"
+
+    @FindBy(how = How.XPATH, using = "//button[.='Stop']")
+    public WebElement stopVote;
+
+    @FindBy(how = How.XPATH, using = "//button[@class='ui-btn ui-btn-lg ui-btn-primary']")
+    public WebElement voteButton; // button to click to vote
+
+    @FindBy(how = How.XPATH, using = "//button[.='Results']")
+    public WebElement pollResultButton; // button to see poll result
+
+    @FindBy(how = How.XPATH, using = "//a[@class='bx-vote-voted-users'][1]")
+    public WebElement votedPeopleNumber; //first web element with first answer to verify poll result
+
+    @FindBy(how = How.XPATH, using = "//button[@class='ui-btn ui-btn-lg ui-btn-link'][1]")
+    public WebElement voteAgain;
+
+
+
+
+
 
 
 
---
+++ src/test/java/UserStory5/VoteMethods.java
@@ -0,0 +1,84 @@
+package UserStory5;
+
+import CRMNextBaseProject.*;
+import CRMNextBaseProject.Driver;
+import java.util.concurrent.TimeUnit;
+
+public class VoteMethods {
+
+    LogIn login = new LogIn();
+    VotePage votePage = new VotePage();
+    LogOut logOut = new LogOut();
+
+
+
+    public void voteUserHr52(){
+        login.loginHR52();
+
+        votePage.answerRadio1.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        votePage.voteButton.click();
+        logOut.setLogOut();
+    }
+
+    public void voteMarketing52() {
+
+        login.loginMarketing52();
+        votePage.answerRadio1.click();
+        votePage.voteButton.click();
+        logOut.setLogOut();
+    }
+
+    public void voteHelpdesk52() {
+
+        login.loginHelpDeck52();
+        votePage.answerRadio1.click();
+        votePage.voteButton.click();
+        logOut.setLogOut();
+    }
+    //method to verify that assigned persons successfully voted(login person who created poll)
+    public void verifyVote(){
+        login.loginHR51();
+        votePage.pollResultButton.click();
+        String numOfPeopleVoted = votePage.votedPeopleNumber.getText();
+        if(!numOfPeopleVoted.equals("0")){
+            System.out.println("Voting verified"+'\n'+numOfPeopleVoted+" answers is showing");
+        }else {
+            System.out.println("0 people picked first answer");
+        }
+        logOut.setLogOut();
+    }
+
+    //method to verify that user can vote again
+    public void voteAgain(){
+        login.loginHR52();
+        votePage.voteAgain.click();
+        ThreadSleep.threadSleep(2);
+        votePage.answerRadio2.click();
+        votePage.voteButton.click();
+        ThreadSleep.threadSleep(2);
+        boolean ableVoteAgain = votePage.voteAgain.isDisplayed();
+        if (ableVoteAgain)
+            System.out.println("User able to vote again");
+        else
+            System.out.println("User wasn't able to vote");
+        logOut.setLogOut();
+    }
+
+    //method to verify that User who create a poll able to stop it
+    public void stopPoll(){
+        login.loginHR51(); //sign in with account who created the poll
+        votePage.stopVote.click();
+        ThreadSleep.threadSleep(1);
+        boolean notAbleVote = (votePage.answerRadio1.isDisplayed());
+        if(!notAbleVote)
+            System.out.println("Poll successfully stopped");
+        else
+            System.out.println("Poll still active");
+        logOut.setLogOut();
+    }
+
+
+
+
+}

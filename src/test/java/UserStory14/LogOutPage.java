b0f45b620b0f5cd6dfd4299247c7ad9d3f0a09f9
Polina <polinavasylega@yahoo.com>
2021-01-25 12:23:09 -0600

    new changes1

---
+++ src/test/java/UserStory14/LogOutPage.java
@@ -0,0 +1,15 @@
+package UserStory14;
+
+import CRMNextBaseProject.HomePage;
+import CRMNextBaseProject.LogIn;
+import org.testng.annotations.Test;
+
+public class LogOutPage {
+    @Test
+    public static void logOut(LogIn logInPage){
+        HomePage homePage1 = new HomePage();
+        homePage1.userNameAvatarButton.click();
+        homePage1.logOutButton.click();
+    }
+
+}
